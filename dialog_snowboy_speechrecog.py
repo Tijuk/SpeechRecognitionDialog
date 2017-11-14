08b38315fc984aba22ce9843a492c56137f6281e
Tijuk <tijuk00@hotmail.com>
2017-11-14 14:34:46 -0200

    not empty

---
+++ dialog_snowboy_speechrecog.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import speech_recognition as sr
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = open("credentials.json").read()
+
+r = sr.Recognizer()
+
+import talk_dialogflow as apiai
+
+def adjust_for_ambient_noise():
+	global r
+	with sr.Microphone() as source:
+		r.adjust_for_ambient_noise(source)
+
+def recognize_google(recognizer, audio, language):
+	l = language
+	text = '-=-'
+	try:
+	    text = recognizer.recognize_google_cloud(audio, language = l, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+	except sr.UnknownValueError:
+	    print("Google Cloud Speech não entendeu o que foi falado")
+	except sr.RequestError as e:
+	    print("Could not request results from Google Cloud Speech service; {0}".format(e))
+	return text;
+
+def callback():
+	global r
+	with sr.Microphone() as source:
+		print('Waiting for Input')
+		audio = r.listen(source)
+		speech = recognize_google(r, audio, "pt-br")
+		action = apiai.query(response)
+        print('Speech is: [%s]' % speech)
+        print('Action is: [%s]' % action)
+
+adjust_for_ambient_noise()
+
+#--------------------------------------------------------
+
+import sys
+#a = open("snowboy_path.txt").read()
+sys.path.append('/home/cyberlab/snowboy/swig/Python')
+import snowboydecoder
+
+import signal
+
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+if len(sys.argv) == 1:
+    print("Error: need to specify model name")
+    print("Usage: python demo.py your.model")
+    sys.exit(-1)
+
+model = sys.argv[1]
+
+# capture SIGINT signal, e.g., Ctrl+C
+signal.signal(signal.SIGINT, signal_handler)
+
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+print('Listening... Press Ctrl+C to exit')
+
+# main loop
+detector.start(detected_callback=callback,
+               interrupt_check=interrupt_callback,
+               sleep_time=0.03)
+
+detector.terminate()
---
+++ talk_dialogflow.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+import os
+import sys
+import json
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            os.pardir,
+            os.pardir
+        )
+    )
+
+    import apiai
+
+
+# demo agent acess token: e5dc21cab6df451c866bf5efacb40178
+
+CLIENT_ACCESS_TOKEN = '65877e113a5b429395d4c4cb4543f867'
+
+
+def query(input):
+    print('quering')
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+    user_message = input
+    if input == '-=-':
+        return
+    request = ai.text_request()
+    request.query = user_message
+
+    response = json.loads(request.getresponse().read())
+
+    result = response['result']
+    action = result.get('action')
+    actionIncomplete = result.get('actionIncomplete', False)
+
+    if action is not None:
+        print(result.get('fulfillment').get('speech'))
+
+    return action
